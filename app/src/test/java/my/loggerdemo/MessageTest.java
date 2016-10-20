da270d55f33869a17e0904a63f3486c0bd242260
Ilian Georgiev <georgiev.ilian@gmail.com>
2016-10-20 13:51:25 +0300

    Add Message class test cases.

--- app/src/test/java/my/loggerdemo/MessageTest.java
+++ app/src/test/java/my/loggerdemo/MessageTest.java
@@ -16,8 +16,84 @@ public class MessageTest {
 
         String result = "{ currentTime: " + currentTime
                 + "; identifier: my.loggerdemo.MessageTest|" +
-                "testMessageCreation_normalString|MessageTest.java|15|1; message:" + logMessage;
-        Message message = new Message(logMessage);
+                "testMessageCreation_normalString|MessageTest.java|21|1; message: " + logMessage + ";}";
+        int callingMethodIndex = 3;
+        Message message = new Message(logMessage, callingMethodIndex);
+        message.setCurrentTime(currentTime);
+
+        assertEquals(result, message.toString());
+    }
+
+    @Test
+    public void testMessageCreation_stringWithQuotes() {
+        String logMessage = "this is ''log it's";
+        long currentTime = System.currentTimeMillis();
+
+        String result = "{ currentTime: " + currentTime
+                + "; identifier: my.loggerdemo.MessageTest|" +
+                "testMessageCreation_stringWithQuotes|MessageTest.java|36|1; message: " + logMessage + ";}";
+        int callingMethodIndex = 3;
+        Message message = new Message(logMessage, callingMethodIndex);
+        message.setCurrentTime(currentTime);
+
+        assertEquals(result, message.toString());
+    }
+
+    @Test
+    public void testMessageCreation_stringWithEscape() {
+        String logMessage = "this is \"hello\" log";
+        long currentTime = System.currentTimeMillis();
+
+        String result = "{ currentTime: " + currentTime
+                + "; identifier: my.loggerdemo.MessageTest|" +
+                "testMessageCreation_stringWithEscape|MessageTest.java|51|1; message: " + logMessage + ";}";
+        int callingMethodIndex = 3;
+        Message message = new Message(logMessage, callingMethodIndex);
+        message.setCurrentTime(currentTime);
+
+        assertEquals(result, message.toString());
+    }
+
+    @Test
+    public void testMessageCreation_null() {
+        String logMessage = null;
+        long currentTime = System.currentTimeMillis();
+
+        String result = "{ currentTime: " + currentTime
+                + "; identifier: my.loggerdemo.MessageTest|" +
+                "testMessageCreation_null|MessageTest.java|66|1;}";
+        int callingMethodIndex = 3;
+        Message message = new Message(logMessage, callingMethodIndex);
+        message.setCurrentTime(currentTime);
+
+        assertEquals(result, message.toString());
+    }
+
+    @Test
+    public void testMessageCreation_emptyString() {
+        String logMessage = "";
+        long currentTime = System.currentTimeMillis();
+
+        String result = "{ currentTime: " + currentTime
+                + "; identifier: my.loggerdemo.MessageTest|" +
+                "testMessageCreation_emptyString|MessageTest.java|81|1; message: ;}";
+        int callingMethodIndex = 3;
+        Message message = new Message(logMessage, callingMethodIndex);
+        message.setCurrentTime(currentTime);
+
+        assertEquals(result, message.toString());
+    }
+
+    @Test
+    public void testMessageCreation_twoSpaces() {
+        String logMessage = "  ";
+        long currentTime = System.currentTimeMillis();
+
+        String result = "{ currentTime: " + currentTime
+                + "; identifier: my.loggerdemo.MessageTest|" +
+                "testMessageCreation_twoSpaces|MessageTest.java|96|1; message:   ;}";
+        int callingMethodIndex = 3;
+        Message message = new Message(logMessage, callingMethodIndex);
         message.setCurrentTime(currentTime);
 
         assertEquals(result, message.toString());
