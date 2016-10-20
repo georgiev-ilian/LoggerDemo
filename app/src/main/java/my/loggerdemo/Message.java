c2d439716149a5c570471de39d2cdc85cfd26101
Ilian Georgiev <georgiev.ilian@gmail.com>
2016-10-20 13:52:49 +0300

    - Add callingMethodIndex as a parameter - Add method for testing

--- app/src/main/java/my/loggerdemo/Message.java
+++ app/src/main/java/my/loggerdemo/Message.java
@@ -1,21 +1,29 @@
 package my.loggerdemo;
 
+import android.support.annotation.VisibleForTesting;
+
 /**
+ * Message class keeps a single log message details. Immutable.
+ *
  * Created by Ilian Georgiev.
  */
 /* package */ class Message {
 
     public static final String DELIMITER = ";";
 
-    private static final int CALLING_METHOD_INDEX = 5;
+    private static final int DEFAULT_CALLING_METHOD_INDEX = 5;
 
     private long currentTime;
     private final Identifier identifier;
     private byte[] byteArr;
 
     public Message(String message) {
+        this(message, DEFAULT_CALLING_METHOD_INDEX);
+    }
+
+    public Message(String message, int callingMethodIndex) {
         this.currentTime = System.currentTimeMillis();
-        this.identifier = new Identifier();
+        this.identifier = new Identifier(callingMethodIndex);
 
         if (message != null) {
             message = message.replace(DELIMITER, " ").replace(Identifier.DELIMITER, " ");
@@ -23,19 +31,16 @@ package my.loggerdemo;
         }
     }
 
-    /**
-     * @return Returns export variant of this message.
-     */
-    public String export() {
-        String message = (byteArr != null) ? new String(byteArr) : "";
-        return currentTime + DELIMITER + identifier + DELIMITER + message;
+    @VisibleForTesting
+    public void setCurrentTime(long currentTime) {
+        this.currentTime = currentTime;
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
 
-        sb.append("{ ").append(" currentTime: ").append(currentTime).append(";");
+        sb.append("{ ").append("currentTime: ").append(currentTime).append(";");
         sb.append(" identifier: ").append(identifier.toString()).append(";");
         if (byteArr != null) {
             sb.append(" message: ").append(new String(byteArr)).append(";");
@@ -51,11 +56,21 @@ package my.loggerdemo;
 
         private final byte[] value;
 
-        public Identifier() {
+        /**
+         * Class constructor.
+         * @param callingMethodIndex Identifies the calling method in the stack trace. This number
+         *                           depends on how nested the call to this constructor happened.
+         *                           As all logging is intended to be used from static method there
+         *                           is a default value to be used
+         *                           {@link #DEFAULT_CALLING_METHOD_INDEX}.
+         */
+        public Identifier(int callingMethodIndex) {
             Thread t = Thread.currentThread();
             StackTraceElement[] stack = t.getStackTrace();
 
-            int k = CALLING_METHOD_INDEX;
+
+
+            int k = callingMethodIndex;
             String str = stack[k].getClassName() + DELIMITER + stack[k].getMethodName()
                     + DELIMITER + stack[k].getFileName() + DELIMITER + stack[k].getLineNumber()
                     + DELIMITER + t.getId();
