ffdb39458bf6e539bdd71b4a1689362823f099ad
Ilian Georgiev <georgiev.ilian@gmail.com>
2016-10-24 17:34:50 +0300

    Add Logger interfaces

---
+++ app/src/main/java/my/loggerdemo/Logger.java
@@ -0,0 +1,23 @@
+package my.loggerdemo;
+
+/**
+ * Created by Ilian Georgiev.
+ */
+
+public interface Logger {
+    /**
+     * Stores a message to the logger
+     * @param message
+     */
+    void log(Message message) throws LoggerException;
+
+    /**
+     * @return Returns collected logs as a string ready to be send.
+     */
+    String export() throws LoggerException;
+
+    /**
+     * Wipe all collected messages in the logger
+     */
+    void clear() throws LoggerException;
+}
---
+++ app/src/main/java/my/loggerdemo/LoggerException.java
@@ -0,0 +1,11 @@
+package my.loggerdemo;
+
+/**
+ * Created by Ilian Georgiev.
+ */
+
+public class LoggerException extends Exception {
+    public LoggerException(String detailMessage) {
+        super(detailMessage);
+    }
+}
