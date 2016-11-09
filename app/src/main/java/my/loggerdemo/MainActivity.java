b176c387f40f6786234aed25c2ce035dc5a80f22
Ilian Georgiev <georgiev.ilian@gmail.com>
2016-11-09 16:25:41 +0200

    Clean up comments.

--- app/src/main/java/my/loggerdemo/MainActivity.java
+++ app/src/main/java/my/loggerdemo/MainActivity.java
@@ -6,14 +6,6 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    //private static LoggerDeque<LoggerMessage> loggerDeque = new LoggerDeque<>(1000);
-
-    //public static final String CONSTANT = "This is constant";
-
-    //public final String[] CONSTANT_ARRAY = new String[100000000];
-
-    //public String myVariable = "This is variable";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,33 +20,14 @@ public class MainActivity extends AppCompatActivity {
         try {
             logger = new LogToFile(this);
             logger.log(new Message("Message #1"));
-            logger.log(new Message("Message #1"));
-            logger.log(new Message("Message #1"));
+            logger.log(new Message("Message #2"));
+            logger.log(new Message("Message #3"));
         } catch (LoggerException e) {
             Log.d("MainActivity", "Cannot create logger and log messages", e);
         }
 
-        /*Iterator<Message> it = Log.getInstance().iterator();
-        Message message;
-
-        while (it.hasNext()) {
-            message = it.next();
-            android.util.Log.d("MainActivity", "onResume: " + message.toString());
-        }*/
-
-/*
-        for (int i = 0; i < 100000; i++) {
-            //Log.a("Hello I'm a logger message to be logged");
-            LogToFile.getInstance(this).log("Message #1");
-        }
-*/
-
-        /*for (int i = 0; i < 100000000; i++) {
-            CONSTANT_ARRAY[i] = "Hello my constant";
-        }*/
-
         if (logger != null) {
-            android.util.Log.d("MainActivity", "onResume: #1");
+            Log.d("MainActivity", "onResume: #1");
 
             try {
                 Log.d("MainActivity", "onResume: " + logger.export());
